a217bd62e2c19a419fdc5538ea36fb20f4d146b3
bread <75878656+broadeditz@users.noreply.github.com>
2023-11-29 02:45:19 +0100

    removed required providers block

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.7.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.18.1"
-    }
-  }
-}
-
 data "kubernetes_namespace" "app" {
   metadata {
     name = var.namespace
